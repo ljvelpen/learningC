c9d3eb195b934e2396b041b95322d9adbdb50b4e
Larissa van der Velpen <larissa@Larissas-MacBook-Air.local>
2026-01-19 16:19:39 +0100

    testing on new Mac

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    printf("Hello World!");
-    return 0;
+    printf("Hello World!\n");
+    return 0;                   //Main always needs to return an integer in order to exit. 0 indicates that the program ran successfully.
 }
 
